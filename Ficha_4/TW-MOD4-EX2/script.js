90de1e3c8578b9663b60f96f51d49d3e9718f223
BensonTerra <benson.terra@hotmail.com>
2022-12-09 15:05:34 +0000

    commit 1.1

---
+++ Ficha_4/TW-MOD4-EX2/script.js
@@ -0,0 +1,15 @@
+var num1 = prompt("Valor para primeiro numero:")
+var num2 = prompt("Valor para segundo numero:")
+
+if (num1 > num1)
+{
+    console.log("Num1 é maior que Num2");
+}
+else if (num2 > num1)
+{
+    console.log("Num2 é maior que Num1");
+}
+else if (num1 == num2)
+{
+    console.log("Os numeros são iguais");
+}
