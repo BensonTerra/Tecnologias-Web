ddccb040fc9bee95e9861fb8755a75c360ac9c59
BensonTerra <benson.terra@hotmail.com>
2022-12-12 15:17:37 +0000

    commit 2

---
+++ Ficha_4/TW-MOD4-EX5-(1)/script.js
@@ -0,0 +1,11 @@
+function  calculateDogAge(idade)
+{
+    var idadeFinal = idade * 7;
+    console.log("Your dog is " + idadeFinal + " years old in dog year");
+
+}
+/*---------------------------------*/
+for(var i = 1; i <=3;i++)
+{
+    calculateDogAge(i);
+}
---
+++ Ficha_4/TW-MOD4-EX5-(2)/script.js
@@ -0,0 +1,6 @@
+let  calculateDogAge = (x) => x * 7
+
+for(var i = 1; i <=3;i++) 
+{
+    console.log("Your dog is " + calculateDogAge(i) + " years old in dog year");
+}
