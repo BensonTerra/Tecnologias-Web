0987a11f895d4a6ee131a4a622c1afbf50816155
BensonTerra <benson.terra@hotmail.com>
2023-01-10 19:24:57 +0000

    prova---2 EX1

--- PROVA---2/EX1/java.js
+++ PROVA---2/EX1/java.js
@@ -1,21 +1,39 @@
-const divB = document.createElement('div')[2];
+const insertBear = document.getElementById("insertBear")
+const removeAll = document.getElementById("removeAllBears")
 
-divB.addEventListener('click', function(event)
-{
+function adicionar() {
     const line = document.createElement('div');
 
     const caption = document.getElementById("caption").value;
-    console.log(caption);
     const bearImage = document.getElementById("image").value;
-    console.log(bearImage);
 
-    line.innerHTML = `<div id="bearsContainer">
+    line.innerHTML = `
+                    <div class="bearFigure">
+                        <div class="bearImage">
+                            <img src="${bearImage}" alt="Random bear" />
+                        </div>
+                        <div class="caption">${caption}</div>
+                    </div>`;
+    
+    const table = document.getElementById('bearsContainer');
+    table.append(line)
+}
+
+function remover() {
+    const line = document.getElementsByTagName('div')[4];
+
+    line.innerHTML = `
+                    <div id="bearsContainer">
                         <div class="bearFigure">
                             <div class="bearImage">
                                 <img src="https://placebear.com/201/200" alt="Random bear" />
                             </div>
                             <div class="caption">Paddington</div>
                         </div>
-                    </div>`
+                    </div>
+                    `
+    
+}
 
-})
+insertBear.addEventListener('click', adicionar)
+removeAll.addEventListener('click', remover)
